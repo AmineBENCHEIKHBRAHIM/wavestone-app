b619ab7af63329f4065f1b08a3733907ae2018fa
AmineBENCHEIKHBRAHIM <amine.ben-cheikh@sprint-je.com>
2018-08-21 01:05:09 +0200

    Login page index.php deisgn creation

--- web/index.php
+++ web/index.php
@@ -1,6 +1,9 @@
 <?php 
 
-echo 'Hello, World!'; echo 'from second pc';
+
+
+
+//echo 'Hello, World!'; echo 'from second pc';
 
 /*if (strpos(getenv('SERVER_SOFTWARE'), 'Development') === 0){
     echo 'Local development server';
@@ -15,6 +18,8 @@ echo 'Hello, World!'; echo 'from second pc';
  -Replace "demo-db" with the name of your instance in Cloud SQL
  -Replace "root" with your user name
  */
+
+/*
 $conn = mysql_connect(getenv('MYSQL_DB'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'));
 if (!$conn) {
     die('Connect Error (' . mysql__error());
@@ -37,18 +42,24 @@ while ($row = mysql_fetch_assoc($result)) {
     echo "<td align=center class=addCommas>" . $row['content'] . "</td></tr>";
 }
 echo "</table>";
+*/
 ?>
 
 
+
+
+
 <!-- Style the results table -->
+<!--  
 <style type="text/css">
 h3 {font-family:verdana;font-size:24px;color:#181C26;}
 table.simpletable {font-family:verdana;font-size:15px;color:#40434A;border-width:1px;border-color:#778AB8;border-collapse:collapse;}
 table.simpletable th {border-width: 1px;padding: 10px;border-style: solid;border-color:#778AB8;background-color:#dedede;}
 table.simpletable td {border-width: 1px;padding: 10px;border-style: solid;border-color: #778AB8;background-color: #ffffff;}
 </style>
-
+-->
 <!-- Add commas to numbers appearing in the table cell with the attribute 'class=addCommas'-->
+<!--  
 <script type="text/javascript">
 function formatNumberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -61,6 +72,28 @@ for (i in elements) {
    }
 }
 </script>
+-->
 
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Input from UI Design</title>
+	<link type="text/css" rel="stylesheet" href="/stylesheets/style.css">
+</head>
+<body>
+	<div class="box">
+		<h2>Login</h2>
+		<form>
+			<div class="inputBox">
+				<input type="text" name="" required="">
+				<label>Username</label>
+			</div>
+			<div class="inputBox">
+				<input type="password" name="" required="">
+				<label>Password</label>
+			</div>
+			<input type="submit" name="" value="submit">
+		</form>
+	</div>
 </body>
 </html>
